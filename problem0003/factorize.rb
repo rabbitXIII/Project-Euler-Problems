e55262852d10132cccd9430e20c7081f05f58985
Rohit Gopal <rohit@collegerail.com>
2013-07-30 23:27:52 +0000

    finds the prime factors for a given number. requires prime to check if a number is a prime, but this could be easily implemented

--- problem0003/factorize.rb
+++ problem0003/factorize.rb
@@ -5,3 +5,32 @@ Rohit Gopal
 
 	  Going to redo implementing a Quadratic Sieve
 =end
+
+require 'prime'
+
+def prime_factorization( number )
+	factors = Array.new
+	potential_factor = 2
+	while number != 1 
+		while number % potential_factor == 0
+			factors[potential_factor] = (factors[potential_factor] || 0) + 1
+			number = number / potential_factor
+			puts number
+		end
+		potential_factor += 1
+	end
+	return factors
+end
+
+def print_prime_factors( factors )
+	1.upto(factors.length-1) do |factor|
+		puts "#{factor} ^ #{factors[factor]}" if factors[factor]
+	end
+end
+
+puts "Which number do you want to factor?"
+number_to_factor = gets.chomp.to_i
+
+factors = prime_factorization(number_to_factor)
+puts "The factors are: "
+print_prime_factors(factors)
